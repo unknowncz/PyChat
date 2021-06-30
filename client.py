f1ba4dd3ee81a9cbdaf43a6da38c6c32851dc272
unknowncz <86740586+unknowncz@users.noreply.github.com>
2021-06-30 20:59:19 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -3,10 +3,14 @@ import socket, time, threading
 connip='127.0.0.1'#input('Connect to: ')
 connport=42069#int(input('On port: '))
 
-def recv(socke:socket.socket):
-    while True:
-        try:print(str(socke.recv(2048), 'UTF-8'))
-        except (ConnectionResetError, ConnectionAbortedError):
+def sendmsg(socke:socket.socket):
+    while socke:
+        try:
+            msg=f'{input()}'
+            socke.sendall(bytes(msg, 'UTF-8'))
+        except (ConnectionAbortedError, ConnectionResetError):break
+        except Exception as e:
+            print(e)
             break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -19,12 +23,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         sock.connect((str(newconn[0]), int(newconn[1])))
         print(sock.recv(256).decode('UTF-8'))
         pname, pport=sock.getpeername()
-        threading.Thread(target=recv, args=(sock,)).start()
-        while sock:
-            try:
-                msg=f'{input()}'
-                sock.sendall(bytes(msg, 'UTF-8'))
+        threading.Thread(target=sendmsg, args=(sock,)).start()
+        while True:
+            try:print(str(sock.recv(2048), 'UTF-8'))
             except (ConnectionResetError, ConnectionAbortedError):
-                print('Connection aborted by server')
                 break
-
