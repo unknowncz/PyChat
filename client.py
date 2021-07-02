9af98228e59e17dd84d6136d0ffe81be56b2da2f
unknowncz <86740586+unknowncz@users.noreply.github.com>
2021-07-02 10:35:06 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,30 +1,88 @@
-import socket, time, threading
+import socket, time, threading, sys, subprocess, msvcrt
+def tryimport():
+    from pyreadline import Readline as readline
+try: import readline
+except:
+    try:tryimport()
+    except:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'pyreadline'])
+        tryimport()
 
-connip='127.0.0.1'#input('Connect to: ')
-connport=42069#int(input('On port: '))
+with open(f'{__file__}/../client.properties') as f:
+    options={}
+    lines=f.readlines()
+    for i in lines:
+        i=','.join(i.split(',')[:-1])
+        option=i.split(':')
+        print(i, option)
+        if option[1].startswith("'"):parsed=option[1].strip("',")
+        elif option[1].startswith("["):parsed=[]
+        elif option[1].startswith('range('):
+            nums=option[1].split('(')[1].split(')')[0].split(', ')
+            parsed=range(int(nums[0]),int(nums[1]))
+        elif len(option[1])>0:
+            try:parsed=int(option[1])
+            except:
+                try:parsed=bool(option[1])
+                except:pass
+        options[option[0]]=parsed
+    f.close()
 
-def sendmsg(socke:socket.socket):
+flushing=False
+msg=''
+
+def getinput():
+    global newmessage
+    sys.stdout.write('> ')
+    sys.stdout.flush()
+    input = ''
+    while True:
+        if msvcrt.kbhit():
+            byte_arr = msvcrt.getch()
+            if ord(byte_arr) == 13: # enter_key
+                sys.stdout.write('\n')
+                sys.stdout.flush()
+                return input
+            elif ord(byte_arr) >= 32: #space_char
+                input += "".join(map(chr,byte_arr))
+                sys.stdout.write(byte_arr.decode())
+                sys.stdout.flush()
+            elif ord(byte_arr) == 8:
+                input = input[:-1]
+                sys.stdout.write(f'{byte_arr.decode()} {byte_arr.decode()}')
+                sys.stdout.flush()
+        if newmessage:
+            sys.stdout.write('\b \b'*(len(input)+2))
+            print(str(newmessage, 'UTF-8'))
+            newmessage=''
+            sys.stdout.write(f'> {input}')
+            sys.stdout.flush()
+
+def sendmsg(socke:socket.socket, *args):
     while socke:
         try:
-            msg=f'{input()}'
-            socke.sendall(bytes(msg, 'UTF-8'))
+            msg=getinput()
+            socke.send(bytes(msg, 'UTF-8'))
         except (ConnectionAbortedError, ConnectionResetError):break
         except Exception as e:
             print(e)
             break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((connip, connport))
+    s.connect((options['connip'], options['connport']))
     time.sleep(0.2)
     newconn=s.recv(256).decode('UTF-8').split(', ')
     time.sleep(0.2)
     s.close()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((str(newconn[0]), int(newconn[1])))
+        if options['alternateconnect']:
+            sock.connect((options['alternateip'], int(newconn[1])))
+        else:
+            sock.connect((str(newconn[0]), int(newconn[1])))
         print(sock.recv(256).decode('UTF-8'))
-        pname, pport=sock.getpeername()
-        threading.Thread(target=sendmsg, args=(sock,)).start()
+        sendthread=threading.Thread(target=sendmsg, args=(sock,)).start()
         while True:
-            try:print(str(sock.recv(2048), 'UTF-8'))
+            try:
+                newmessage:bytes=sock.recv(2048)
             except (ConnectionResetError, ConnectionAbortedError):
                 break
--- server.py
+++ server.py
@@ -2,12 +2,24 @@ import socket, random, threading
 
 print('Starting...')
 
-sendserverip='127.0.0.1'                # ip of message server (to be sent to client)
-actualserverip=''                       # ip of server to be bound
-queueip=''                              # ip of queue server (client connects to get server + port)
-serverports=range(40000,42068)          # range of available server ports
-queueport=42069                         # queue port for connections
-threads=[]                              # active threads
+with open(f'{__file__}/../server.properties') as f:
+    options={}
+    lines=f.readlines()
+    for i in lines:
+        i=','.join(i.split(',')[:-1])
+        option=i.split(':')
+        if option[1].startswith("'"):parsed=option[1].strip("',")
+        elif option[1].startswith("["):parsed=[]
+        elif option[1].startswith('range('):
+            nums=option[1].split('(')[1].split(')')[0].split(', ')
+            parsed=range(int(nums[0]),int(nums[1]))
+        elif len(option[1])>0:
+            try:parsed=int(option[1])
+            except:
+                try:parsed=bool(option[1])
+                except:pass
+        options[option[0]]=parsed
+    f.close()
 
 def connmanager(serverip, newport, sentserverip):
     global commmgr, portmgr
@@ -82,16 +94,16 @@ class portmanager():
 
 
 commmgr=commmanager()
-portmgr=portmanager(serverports) #init a port manager
-print(f'Server started on {sendserverip}.')
+portmgr=portmanager(options['serverports']) #init a port manager
+print(f'Server started on {options["sendserverip"]}.')
 while True:
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-        s.bind((queueip, queueport))
+        s.bind((options['queueip'], options['queueport']))
         s.listen(100) #listen for connections on the queue server (port)
         x,y=s.accept()
         with x:
             newport=portmgr.get() #get a new port to connect to
-            x.sendall(bytes(str(f'{sendserverip}, {str(newport)}'), 'UTF-8')) #send new ip and port to client
-            newthread=threading.Thread(target=connmanager,args=(actualserverip, newport, sendserverip),name=f'{sendserverip}:{newport}') #start new thread to handle imminent client connection
+            x.sendall(bytes(str(f'{options["sendserverip"]}, {str(newport)}'), 'UTF-8')) #send new ip and port to client
+            newthread=threading.Thread(target=connmanager,args=(options['actualserverip'], newport, options['sendserverip']),name=f'{options["sendserverip"]}:{newport}') #start new thread to handle imminent client connection
             newthread.start() #start the thread
-            threads.append(newthread)
+            options['threads'].append(newthread)
